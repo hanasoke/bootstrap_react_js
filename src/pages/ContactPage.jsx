75c0cbdd065daadb12958be7890a28d42e937f06
Hanas <hanasoke@gmail.com>
2026-02-28 21:00:18 +0700

    updated pop up ContactPage.jsx

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -216,24 +216,28 @@ const ContactPage = () => {
                     <div className="mb-3 row">
                       <label className="col-sm-4 col-form-label fw-bold">Email</label>
                       <div className="col-sm-8">
-                        <p className="form-control-plaintext">saitama@gmail.com</p>
+                        <p className="form-control-plaintext">{formData.email || '-'}</p>
                       </div>
                     </div>
                     <div className="mb-3 row">
                       <label className="col-sm-4 col-form-label fw-bold">Phone Number</label>
                       <div className="col-sm-8">
-                        <p className="form-control-plaintext">087322132321</p>
+                        <p className="form-control-plaintext">{formData.phone || '-'}</p>
                       </div>
                     </div>
                     <div className="mb-3 row">
                       <label className="col-sm-4 col-form-label fw-bold">Mesage</label>
                       <div className="col-sm-8">
-                        <p className="form-control-plaintext">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque perspiciatis provident, possimus modi at accusamus temporibus ex magni voluptatem, iusto cumque veritatis officia. Amet pariatur ipsam sunt in vero distinctio!</p>
+                        <p className="form-control-plaintext">{formData.message || '-'}</p>
                       </div>
                     </div>
                   </div>
                   <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button 
+                      type="button" 
+                      className="btn btn-secondary" 
+                      onClick={handleCloseModal}
+                    >Close</button>
                   </div>
                 </div>
               </div>
