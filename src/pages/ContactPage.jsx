4daf9417a77c24a50846189752177d4ac33a1d3f
Hanas <hanasoke@gmail.com>
2026-02-28 17:20:56 +0700

    updated ContactPage.jsx

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -12,7 +12,7 @@ const ContactPage = () => {
       {/* Contact Section */}
       <section id="contact" className="py-5 bg-light">
         <div className="container">
-          <h2 className="text-center mb-5">Hubungi Kami - {roomData.location}</h2>
+          <h2 className="text-center mb-5">Hubungi Kami</h2>
           <div className="row justify-content-center">
             <div className="col">
               <div className="card shadow">
@@ -38,12 +38,54 @@ const ContactPage = () => {
                       <label htmlFor="message" className="form-label">Pesan</label>
                       <textarea className="form-control" id="message" rows="4"></textarea>
                     </div>
-                    <button type="submit" className="btn btn-success w-100">Kirim Pesan</button>
+                    <button type="button" className="btn btn-success w-100" data-bs-toggle="modal" data-bs-target="#exampleModal">Kirim Pesan</button>
                   </form>
                 </div>
               </div>
             </div>
           </div>
+
+          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Detail Pesan</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <div className="mb-3 row">
+                    <label className="col-sm-4 col-form-label fw-bold">Nama</label>
+                    <div className="col-sm-8">
+                      <p className="form-control-plaintext">Saitama</p>
+                    </div>
+                  </div>
+                  <div className="mb-3 row">
+                    <label className="col-sm-4 col-form-label fw-bold">Email</label>
+                    <div className="col-sm-8">
+                      <p className="form-control-plaintext">saitama@gmail.com</p>
+                    </div>
+                  </div>
+                  <div className="mb-3 row">
+                    <label className="col-sm-4 col-form-label fw-bold">Phone Number</label>
+                    <div className="col-sm-8">
+                      <p className="form-control-plaintext">087322132321</p>
+                    </div>
+                  </div>
+                  <div className="mb-3 row">
+                    <label className="col-sm-4 col-form-label fw-bold">Mesage</label>
+                    <div className="col-sm-8">
+                      <p className="form-control-plaintext">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque perspiciatis provident, possimus modi at accusamus temporibus ex magni voluptatem, iusto cumque veritatis officia. Amet pariatur ipsam sunt in vero distinctio!</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
           <div className="row mt-4">
             <div className="col-md-6">
               <h5>Alamat</h5>
